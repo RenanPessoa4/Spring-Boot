5e08cb271a7a4977f57e3b99e36f2c0a0a17f20e
RenanPessoa4 <70712754+RenanPessoa4@users.noreply.github.com>
2020-10-16 00:00:25 -0300

    Update BlogPessoal

---
+++ metodosave/src/main/java/org/metodosave/MetodosaveApplication.java
@@ -0,0 +1,13 @@
+package org.metodosave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetodosaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetodosaveApplication.class, args);
+	}
+
+}
---
+++ metodosave/src/test/java/org/metodosave/MetodosaveApplicationTests.java
@@ -0,0 +1,13 @@
+package org.metodosave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetodosaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
